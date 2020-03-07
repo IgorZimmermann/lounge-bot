08d5d491d1349d801758f46f50b1cfc764a33598
order66executor <kovacs.erik.hu@gmail.com>
2020-03-07 22:08:40 +0100

    fixed ban

--- commands/ban.js
+++ commands/ban.js
@@ -1,7 +1,7 @@
 const discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-	let reportChannel = bot.channels.get('685944369500782625');
+	let reportChannel = bot.channels.fetch('685944369500782625');
 	let reason = message.content.replace(`.ban ${args[0]} `, '');
 	if (!args[0])
 		return message.channel.send('You have to specify the user to ban.');
