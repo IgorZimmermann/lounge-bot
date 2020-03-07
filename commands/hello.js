d6162ccf66894c152e76966f67c13c52697fef66
PaperKing01 <neumann.technologies@gmail.com>
2020-03-07 16:28:42 +0100

    Recreated hello command using createCommand.js

--- commands/hello.js
+++ commands/hello.js
@@ -1,11 +1,12 @@
-module.exports.run = async (bot,message,args) =>{
-    let author = message.author 
-    message.channel.send(`Hello, ${author}!`)
-}
-
-
+module.exports.run = async (bot, message, args) => {
+	let author = message.author;
+	message.channel.send(`Hello, ${author}!`);
+};
 
 module.exports.help = {
-    name: "hello",
-    aliases: ["hi"]
-}
+	name: 'hello',
+	aliases: ['hi', 'howdy', 'hola'],
+	description: 'The bot says hello to you.',
+	hasAccess: 'Everyone',
+	usage: '.hello'
+};
