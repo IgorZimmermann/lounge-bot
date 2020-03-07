983e202d00d1745a5cf807a0aa27615d6970b010
order66executor <kovacs.erik.hu@gmail.com>
2020-03-07 18:20:12 +0100

    repeat first added

---
+++ commands/repeat.js
@@ -0,0 +1,9 @@
+module.exports.run = async (bot, message, args) => {
+    let author = message.author
+    let content = message.content.replace(".repeat", " ")
+    message.channel.send(`${author}: ${content}`)
+
+
+}
+
+module.exports.help = {"name":"repeat","aliases":[""],"description":"Reapeats what the author said","hasAccess":"Everyone","usage":".repeat"}
