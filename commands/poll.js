054634e44cee20ae7879f1de5c2b1e1463dab9a0
PaperKing01 <neumann.technologies@gmail.com>
2020-03-13 20:21:16 +0100

    Simplified ux with messageCollector

--- commands/poll.js
+++ commands/poll.js
@@ -11,15 +11,50 @@ module.exports.run = async (bot, message, args) => {
 			`${message.author}: Created a new poll called ${name}. View details with \`.poll view ${name}\`.`
 		);
 	} else if (args[0] == 'view') {
-		let name = args[1];
-		let poll = bot.polls.getPoll(name);
-
-		let pollEmbed = new discord.MessageEmbed()
-			.setTitle(name)
+		let polls = bot.polls.getAllPolls();
+		let pollsEmbed = new discord.MessageEmbed()
 			.setAuthor(bot.user.username)
-			.setColor('#4FB1BB');
-		poll.options.forEach(o => pollEmbed.addField(o.name, o.votes));
-		message.channel.send(pollEmbed);
+			.setColor('#50B3BC')
+			.setDescription(
+				"Choose the poll you want to view. Reply with it's number!"
+			);
+		polls.forEach((p, i) => {
+			pollsEmbed.addField(
+				`**${i + 1} > ${p.name}**`,
+				bot.polls.getAllOptionNames(p.name).join(',\n')
+			);
+		});
+
+		message.channel.send(pollsEmbed);
+		let collector = message.channel.createMessageCollector(
+			m => {
+				return (
+					m.author.id == message.author.id &&
+					new RegExp(`^([1-${Number(polls.length + 1)}]|cancel)$`, 'i').test(
+						m.content
+					)
+				);
+			},
+			{ time: 20000, max: 1 }
+		);
+
+		collector.on('collect', m => {
+			if (/cancel/i.test(m.content)) return collector.stop('cancelled');
+			let id = m.content;
+			let poll = bot.polls.getAllPolls()[id - 1];
+
+			let pollEmbed = new discord.MessageEmbed()
+				.setTitle(poll.name)
+				.setAuthor(bot.user.username)
+				.setColor('#4FB1BB');
+			poll.options.forEach(o => pollEmbed.addField(o.name, o.votes));
+			message.channel.send(pollEmbed);
+		});
+
+		collector.on('end', (_, reason) => {
+			if (['time', 'cancelled'].includes(reason))
+				return message.channel.send(`${message.author}: Cancelled selection.`);
+		});
 	}
 };
 
