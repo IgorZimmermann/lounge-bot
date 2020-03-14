3371d826ea4eb2f5c4c79897604a00f1245c9c0b
PaperKing01 <neumann.technologies@gmail.com>
2020-03-14 17:33:51 +0100

    Removed config/secrets.json from index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const discord = require('discord.js');
 const fs = require('fs');
 
 const config = require('./config/config.json');
-const secret = require('./config/secrets.json');
 
 let bot = new discord.Client();
 
