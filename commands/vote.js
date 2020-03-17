9541396a9f69d7b046a3f1d2fc6ee25a30bac6d5
PaperKing01 <neumann.technologies@gmail.com>
2020-03-17 16:13:19 +0100

    Both poll and vote commands are working

--- commands/vote.js
+++ commands/vote.js
@@ -5,6 +5,10 @@ module.exports.run = async (bot, message, args) => {
 	if (args[0]) {
 		let name = args.join(' ');
 		Poll.findOne({ title: name }, (err, poll) => {
+			if (!poll)
+				return message.channel.send(
+					`${message.author}: No poll found with that name.`
+				);
 			let pollEmbed = new discord.MessageEmbed()
 				.setAuthor(bot.user.username)
 				.setColor('#50B3BC')
@@ -35,6 +39,55 @@ module.exports.run = async (bot, message, args) => {
 			});
 		});
 	} else {
+		const filter = m => m.author.id == message.author.id;
+		const options = {
+			time: 50000,
+			max: 1,
+			errors: ['time']
+		};
+		message.channel.send('Which poll would you like to vote on?').then(() => {
+			message.channel.awaitMessages(filter, options).then(c => {
+				if (c.first().content == 'cancel')
+					return message.channel.send(`${message.author}: Cancelled`);
+				title = c.first().content;
+				Poll.findOne({ title: title }, (err, poll) => {
+					if (!poll)
+						return message.channel.send(
+							`${message.author}: No poll found with that name.`
+						);
+					let pollEmbed = new discord.MessageEmbed()
+						.setAuthor(bot.user.username)
+						.setColor('#50B3BC')
+						.setTitle(poll.title)
+						.setDescription(
+							"Choose the the choice you want to vote for and send a message with it's number."
+						);
+
+					poll.choices.forEach((p, i) => {
+						pollEmbed.addField(`${i + 1} > ${p.title}`, p.votes);
+					});
+					message.channel.send(pollEmbed).then(() => {
+						const filter = m =>
+							m.author.id == message.author.id &&
+							new RegExp(`^([1-${poll.choices.length}]|cancel)$`).test(
+								m.content
+							);
+
+						const options = { time: 20000, max: 1, errors: ['time'] };
+						message.channel.awaitMessages(filter, options).then(m => {
+							if (/cancel/i.test(m.first().content))
+								return message.channel.send(`${message.author}: Cancelled.`);
+							poll.choices[m.first().content - 1].votes++;
+							Poll.findOneAndUpdate({ title: title }, poll, (err, doc) => {
+								message.channel.send(
+									`Successful vote! View current standing with \`.poll view ${doc.title}\``
+								);
+							});
+						});
+					});
+				});
+			});
+		});
 	}
 };
 
