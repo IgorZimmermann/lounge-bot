38c6543ae8b31d33e1f3b3bc9fa6919699618ba6
Szonja Rozsa <igorzimmermann01@gmail.com>
2021-01-30 17:29:57 +0100

    Tiny reworks on the ban command

--- commands/ban.js
+++ commands/ban.js
@@ -1,27 +1,25 @@
 const discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
-	let reason = message.content.replace(`.ban ${args[0]} `, '')
+	let reason = args.slice(1).join(' ')
 	if (!args[0])
 		return message.channel.send('You have to specify the user to ban.')
 	if (!reason) return message.channel.send('You have to specify a reason.')
-	let hasPermission = message.member.hasPermission('ADMINISTRATOR')
+	let hasPermission = message.member.hasPermission('ADMINISTRATOR') || !message.author.hasPermission('ADMINISTRATOR') 
 	if (!hasPermission)
 		return message.channel.send(
 			"You don't have permissions to use this command. You can report a person with `.report`."
 		)
 	let toBan = message.mentions.members.first()
 	toBan.ban({ reason: reason }).then(() => {
-		let reportEmbed = new discord.MessageEmbed()
+		let banEmbed = new discord.MessageEmbed()
 			.setTitle('Ban')
 			.setColor('RED')
 			.addField('Banned User', toBan)
 			.addField('Banned by', message.author)
 			.addField('Banned at', message.createdAt)
 			.addField('Reason', reason)
-		bot.channels.fetch('685944369500782625').then(c => {
-			c.send(reportEmbed)
-		})
+		message.channel.send(banEmbed)
 	})
 	message.delete()
 }
